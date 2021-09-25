b8800742f8959190377d22a61c4f26c34b287081
qiyi <2816455060@qq.com>
2021-09-26 00:54:08 +0800

    冒泡排序

---
+++ src/main/java/top/qiyi/java/basic/sort/BubbleSort.java
@@ -0,0 +1,38 @@
+package top.qiyi.java.basic.sort;
+
+/**
+ * @program：JavaStudy
+ * @description: 冒泡排序
+ * @author: qiyi
+ * <p>
+ * create: 2021-09-26 00:41
+ **/
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] a, int n) {
+        int i, j;
+        for (i = 0; i < n - i; i++) {
+            for (j = 1; j < n - i; j++) {
+                //前面的数字大于后面的数字就交换
+                if (a[j - 1] > a[j]) {
+                    //交换a[j-1]和a[j]
+                    int temp;
+                    temp = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = temp;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 10, 29, 3, 4, 9, 98, 30};
+        BubbleSort.bubbleSort(arr, arr.length);
+        for (int i : arr) {
+            System.out.print(i + ",");
+        }
+    }
+}
+
